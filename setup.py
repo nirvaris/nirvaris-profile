68acec41cb9ef912d25fe32f8ef75de6f3d9c8f1
JB <juliano.binder@gmail.com>
2015-10-09 14:55:13 +0100

    install

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='nirvaris-profile',
-    version='0.1',
+    version='0.2',
     packages=['profile'],
     include_package_data=True,
     license='MIT License',  # example license
