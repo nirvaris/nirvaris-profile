977bf47ccd6fa8f9b0057f5687b3f1d887af8015
jb <jb@JB-MAC.local>
2017-03-29 17:07:14 +0100

    User photo

--- n_profile/admin.py
+++ n_profile/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import UserDetails
+from .models import UserPhoto
 
-admin.site.register(UserDetails)
+admin.site.register(UserPhoto)
